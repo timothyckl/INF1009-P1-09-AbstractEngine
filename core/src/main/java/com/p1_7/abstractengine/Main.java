a09c9accddfe2e64f56c0b457c882f92aaba2485
timothyckl <timothy.ckl@outlook.com>
2026-01-19 20:54:12 +0800

    set up entity manager instance

--- core/src/main/java/com/p1_7/abstractengine/Main.java
+++ core/src/main/java/com/p1_7/abstractengine/Main.java
@@ -3,14 +3,15 @@ package com.p1_7.abstractengine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import com.p1_7.abstractengine.*;
+import com.p1_7.abstractengine.managers.impl.EntityManager;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    EntityManager entityManager;
 
     @Override
     public void create() {
-
+        entityManager = new EntityManager();
     }
 
     @Override
