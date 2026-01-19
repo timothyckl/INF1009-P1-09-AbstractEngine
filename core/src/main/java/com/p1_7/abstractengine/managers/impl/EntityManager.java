390a68d07ef871e15a59e89c5127dccdd8b650e1
timothyckl <timothy.ckl@outlook.com>
2026-01-19 20:53:48 +0800

    update entity manager methods

--- core/src/main/java/com/p1_7/abstractengine/managers/impl/EntityManager.java
+++ core/src/main/java/com/p1_7/abstractengine/managers/impl/EntityManager.java
@@ -1,14 +1,44 @@
 package com.p1_7.abstractengine.managers.impl;
 
+import java.util.Collection;
+import java.util.UUID;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.p1_7.abstractengine.core.Entity;
 import com.p1_7.abstractengine.managers.base.AbstractManager;
 
-public class EntityManager extends AbstractManager{
-    @Override
-    public void init() {}
+public class EntityManager extends AbstractManager {
+    private ObjectMap<UUID, Entity> entities;
+
+    public EntityManager() {
+        entities = new ObjectMap<>();
+    }
 
     @Override
-    public void update(float deltaTime) {}
+    public void init() {
+
+    }
 
     @Override
-    public void shutdown() {}
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    public Entity getEntity(UUID id) {
+        return entities.get(id);
+    }
+
+    public void addEntity(Entity entity) {
+        entities.put(entity.getID(), entity);
+    }
+
+    public void removeEntity(UUID id) {
+        entities.remove(id);
+    }
 }
