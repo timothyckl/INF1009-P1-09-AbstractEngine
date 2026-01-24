67473fadaa8b67b429b99af30529312c0d4b69a3
timothyckl <timothy.ckl@outlook.com>
2026-01-24 13:16:16 +0800

    update formatting

--- core/src/main/java/com/p1_7/abstractengine/core/AbstractProperty.java
+++ core/src/main/java/com/p1_7/abstractengine/core/AbstractProperty.java
@@ -1,13 +1,13 @@
 package com.p1_7.abstractengine.core;
 
-public abstract class AbstractProperty{
+public abstract class AbstractProperty {
     protected boolean enabled;
 
-    public boolean isEnabled(){
+    public boolean isEnabled() {
         return enabled;
     }
 
-    public void setEnabled(boolean enabled){
+    public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 }
