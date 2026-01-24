4b75cd7db0264c79bd96864af626e65a766039d3
timothyckl <timothy.ckl@outlook.com>
2026-01-24 20:37:02 +0800

    remove deprecated enum-based event types

--- core/src/main/java/com/p1_7/abstractengine/events/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.p1_7.abstractengine.events;
-
-/**
- * Marker interface for event type enums.
- * All event type enums should implement this interface.
- */
-public interface EventType {
-
-}
